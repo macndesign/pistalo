cc2cf5dd62140f07fa5a846278e1a42d4910211e
mario <macndesign@gmail.com>
2013-10-23 11:29:43 -0300

    retirado memcached

--- pistalo/settings.py
+++ pistalo/settings.py
@@ -165,28 +165,3 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_SUBJECT_PREFIX = '[Pistalo]'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-
-# Memcached
-def get_cache():
-    try:
-        os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS']
-        os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
-        os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
-        return {
-            'default': {
-                'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-                'LOCATION': os.environ['MEMCACHIER_SERVERS'],
-                'TIMEOUT': 500,
-                'BINARY': True,
-            }
-        }
-    except:
-        return {
-            'default': {
-                'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-            }
-        }
-
-
-CACHES = get_cache()
